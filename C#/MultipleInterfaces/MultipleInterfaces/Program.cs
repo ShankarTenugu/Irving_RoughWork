8ebd0368d214c7afebe8732fbe5896576e04698a
Shankar Tenugu <stenugu@acecashexpress.com>
2018-03-16 11:24:41 -0500

    multipleInterfaces

---
+++ C#/MultipleInterfaces/MultipleInterfaces/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultipleInterfaces
+{
+    interface IFileLog
+    {
+        string writeLog();
+    }
+
+    interface IDataBaseLog
+    {
+        string writeLog();
+    }
+
+    class MyLogClass:IFileLog,IDataBaseLog
+    {
+        public string writeLog() //it should have 'public' access specifier
+        {
+            return "I am logger method";
+        }
+
+        string IFileLog.writeLog()
+        {
+            return "I am logger method from IFileLog";
+        }
+
+        string IDataBaseLog.writeLog()
+        {
+            return "I am logger method from IDataBaseLog";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyLogClass _log = new MyLogClass();
+            IFileLog _logF = new MyLogClass();
+            IDataBaseLog _logD = new MyLogClass();
+
+            Console.WriteLine("Logger File from object" + _log.writeLog());
+            Console.WriteLine("Logger File from object" + _logF.writeLog());
+            Console.WriteLine("Logger File from object" + _logD.writeLog());
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
